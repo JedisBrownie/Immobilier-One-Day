2feb54cd44e5792d070d11a8210b0d64587d7742
Judichael73 <120610522+Judichael73@users.noreply.github.com>
2022-12-15 19:54:41 +0100

    nanampy sary sy nanao modification

---
+++ listHabAdmin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Document</title>
+</head>
+<body>
+    <div id="sary1"><img src="Pic/image2.jpg" width = "1000px;"></div>
+    <div id="titre">
+        <h2 id="title1" >The villa rental expert for your travel.</h2>
+        <h1 id="title2">Book your villa online quickly and safely</h1>
+
+        <h3 id="title3">Homes Around the World</h3>
+        <p id="title4">Discover beautiful homies with all the comforts of home.</p>
+    </div>
+
+</body>
+</html>
