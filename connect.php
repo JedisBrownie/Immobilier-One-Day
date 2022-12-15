9ef8413c03ba0b1bbfdc8430526a9688a3c43e39
TokyHenintsoa18 <tokyramanalina@gmail.com>
2022-12-16 00:37:47 +0300

    update into postgre

--- connect.php
+++ connect.php
@@ -7,14 +7,15 @@
     // {
     //     echo $th;
     // }   
-    try 
-    {
-        $ODBCConnection = odbc_connect("Driver={Devart ODBC driver for Oracle};Direct=true;Host=localhost;Port=1521;Service_Name=orcl;User_ID=CLRExtProc;password=Mdpprom2", "immobilier", "immobilier");
-        echo $ODBCConnection;
-    } 
-    catch (\Throwable $th) 
-    {
-        echo $th;
-    }
+    // try 
+    // {
+    //     $ODBCConnection = odbc_connect("Driver={Devart ODBC driver for Oracle};Direct=true;Host=localhost;Port=1521;Service_Name=orcl;User_ID=CLRExtProc;password=Mdpprom2", "immobilier", "immobilier");
+    //     echo $ODBCConnection;
+    // } 
+    // catch (\Throwable $th) 
+    // {
+    //     echo $th;
+    // }
     
+            
 ?>
--- data.sql
+++ data.sql
@@ -4,9 +4,9 @@ grant dba to immobilier;
 -- User
 CREATE TABLE users
 (
-    idUser varchar(20),
+    idUser serial,
     Nom varchar(20),
-    Mpd varchar(50),
+    Mdp varchar(50),
     Email varchar(50),
     estAdmin int,
     Constraint id_User PRIMARY KEY(idUser)
@@ -15,7 +15,7 @@ CREATE TABLE users
 -- Habitation
 create table habitation
 (
-    idHabitation varchar(20),
+    idHabitation serial,
     Quartier varchar(50),
     Salon int,
     Chambre int,
@@ -28,58 +28,73 @@ create table habitation
 -- Reservation
 create table reservation
 (
-    idReservation varchar(20),
+    idReservation serial
     idHabitation varchar(20),
-    idUser varchar(20),
+    idUser int,
     DateDebutReservation timestamp(1),
     DateFinReservation timestamp(1)
+    FOREIGN KEY(idHabitation)REFERENCES habitation(idHabitation),
+    FOREIGN KEY(idUser)REFERENCES users(idUser)
 );
-
-ALTER TABLE Reservation
-ADD Constraint ID_Habitations
-FOREIGN KEY(idHabitation)
-REFERENCES Habitation(idHabitation);
-
-ALTER TABLE Reservation 
-ADD Constraint id_users 
-FOREIGN KEY (idUser)
-REFERENCES users(idUser);
-
 
 --------SEQUENCE USER-----------------------
 CREATE SEQUENCE USER_SEQ MINVALUE 0 START WITH 0 INCREMENT BY 1 NOCACHE NOCYCLE;
 --------INSERTION USER---------------------
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Toky','toky123456','tokyramanalina@gamil.com',0);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Judichael','judi123456','judichael@gamil.com',0);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Natana','natana123456','natana@gamil.com',0);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Rudy','rudy123456','rudy@gamil.com',1);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Lory','lory123456','lory@gamil.com',1);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Loic','loic123456','loic@gamil.com',1);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Chris','chris123456','chris@gamil.com',1);
-INSERT INTO USERS VALUES(USER_SEQ.NEXTVAL,'Paul','paul123456','paul@gamil.com',1);
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin) 
+VALUES('Toky','toky123456','tokyramanalina@gamil.com',0);
 
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Judichael','judi123456','judichael@gamil.com',0);
 
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Natana','natana123456','natana@gamil.com',0);
 
------------SEQUENCE HABITATION----------------
-CREATE SEQUENCE HAB_SEQ MINVALUE 0 START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Rudy','rudy123456','rudy@gamil.com',1);
+
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Lory','lory123456','lory@gamil.com',1);
+
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Loic','loic123456','loic@gamil.com',1);
+
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Chris','chris123456','chris@gamil.com',1);
+
+INSERT INTO USERS(Nom,Mdp,Email,estAdmin)
+VALUES('Paul','paul123456','paul@gamil.com',1);
+
 -------------INSERTION HABITAION--------------
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Analakely',1,4,1,2,2);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Behoririka',2,5,1,1,1);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Itaosy',1,1,1,1,1);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Andraharo',1,3,1,1,1);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Adoharanofotsy',2,2,4,2,2);
+INSERT INTO HABITATION(Quartier,Salon,Chambre,Douche,Toilette,Balcon) 
+VALUES('Analakely',1,4,1,2,2);
+INSERT INTO HABITATION(Quartier,Salon,Chambre,Douche,Toilette,Balcon) 
+VALUES('Behoririka',2,5,1,1,1);
+INSERT INTO HABITATION(Quartier,Salon,Chambre,Douche,Toilette,Balcon) 
+VALUES('Itaosy',1,1,1,1,1);
+INSERT INTO HABITATION(Quartier,Salon,Chambre,Douche,Toilette,Balcon) 
+VALUES('Andraharo',1,3,1,1,1);
+INSERT INTO HABITATION(Quartier,Salon,Chambre,Douche,Toilette,Balcon) 
+VALUES('Adoharanofotsy',2,2,4,2,2);
 
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Ankadidramama',1,2,1,2,2);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Ankorondrano',1,5,2,2,2);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Ankadifotsy',1,1,1,1,1);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Anosizato',2,2,2,2,1);
-INSERT INTO HABITATION VALUES(HAB_SEQ.NEXTVAL,'Ambohimiadana',1,1,2,2,1);
+INSERT INTO HABITATION (Quartier,Salon,Chambre,Douche,Toilette,Balcon)
+VALUES('Ankadidramama',1,2,1,2,2);
+INSERT INTO HABITATION (Quartier,Salon,Chambre,Douche,Toilette,Balcon)
+VALUES('Ankorondrano',1,5,2,2,2);
+INSERT INTO HABITATION (Quartier,Salon,Chambre,Douche,Toilette,Balcon)
+VALUES('Ankadifotsy',1,1,1,1,1);
+INSERT INTO HABITATION (Quartier,Salon,Chambre,Douche,Toilette,Balcon)
+VALUES('Anosizato',2,2,2,2,1);
+INSERT INTO HABITATION (Quartier,Salon,Chambre,Douche,Toilette,Balcon)
+VALUES('Ambohimiadana',1,1,2,2,1);
 
-----------------SEQUENCE RESRVATION--------------
-CREATE SEQUENCE RESERV_SEQ MINVALUE 0 START WITH 0 INCREMENT BY 1 NOCACHE NOCYCLE;
 --------------------INSERT RESERVATION-------------
-INSERT INTO RESERVATION VALUES(RESERV_SEQ.NEXTVAL,'1','3',' 06-DEC-22 04.33.51.04 PM','12-DEC-22 09.16.36.47 AM');
-INSERT INTO RESERVATION VALUES(RESERV_SEQ.NEXTVAL,'2','5',' 12-DEC-22 11.33.51.04 PM','13-DEC-22 09.46.00.47 AM');
-INSERT INTO RESERVATION VALUES(RESERV_SEQ.NEXTVAL,'5','4',' 25-DEC-22 11.33.51.04 AM','31-DEC-22 09.46.00.47 AM');
-INSERT INTO RESERVATION VALUES(RESERV_SEQ.NEXTVAL,'9','6',' 25-DEC-22 11.33.51.04 AM','31-DEC-22 09.46.00.47 AM');
-INSERT INTO RESERVATION VALUES(RESERV_SEQ.NEXTVAL,'7','7',' 14-OCT-22 07.33.51.04 AM','22-DEC-22 12.46.00.47 AM');
+INSERT INTO RESERVATION(idHabitation,idUser,DateDebutReservation,DateFinReservation)
+VALUES('1','3','06/12/2022 04:33:51.04','12/12/2022 21:16:36.47');
+INSERT INTO RESERVATION (idHabitation,idUser,DateDebutReservation,DateFinReservation)
+VALUES('2','5',' 12/12/2022 11:33:51.04','13-12/2022 12:46:00.47 ');
+INSERT INTO RESERVATION (idHabitation,idUser,DateDebutReservation,DateFinReservation)
+VALUES('5','4',' 25/12/2022 11:33:51.04','31/12/2022 09:46:00.47');
+INSERT INTO RESERVATION (idHabitation,idUser,DateDebutReservation,DateFinReservation)
+VALUES('9','6',' 25/11/2022 11:33:51.04','31/11/2022 07:00:00.00 ');
+INSERT INTO RESERVATION (idHabitation,idUser,DateDebutReservation,DateFinReservation)
+VALUES('7','7',' 01/10/2022 07:33:51.04','11/11/2022 12:46:00.47 ');
