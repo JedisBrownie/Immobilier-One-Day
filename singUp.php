71eac498e02ba8b2651a0d7e8180a56b5b89a039
TokyHenintsoa18 <tokyramanalina@gmail.com>
2022-12-16 08:06:57 +0300

    change

---
+++ singUp.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="singUp.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="box">
+            <div class="form">
+                <h2>Sing Up</h2>
+                <div class="inputBox">
+                            <input type="text" required="required" name="nom">
+                            <span>Username</span>
+                            <i></i>
+                        </div>
+                    
+                        <div class="inputBox">
+                            <input type="password" required="required" name="pwd">
+                            <span>Password</span>
+                            <i></i>
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="email" required="required" name="mail">
+                            <span>Password</span>
+                            <i></i>
+                        </div>
+                        
+                            <input type="submit" value="Valider">                            
+                        <div class="links">
+                            <a href="index.php">RETURN</a>
+                        </div>
+            </div>
+    </div>
+</body>
+</html>
