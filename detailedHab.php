61803ad289e4226a205d02952b2f149cd11a51bf
Judichael73 <120610522+Judichael73@users.noreply.github.com>
2022-12-16 04:55:20 +0100

    modif css

--- detailedHab.php
+++ detailedHab.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>detail habitation</title>
 </head>
 <body>
-    
+    <div id="menu">
+        <div id="logo"><img src="Pic/th.jpg" width="100px" heigth="75px"></div>
+        <div id="recherche">
+            <form action="treatRecherche.php">
+            <input type="text" placeholder="Commencez votre recherche">
+            <input type="submit" value="Cliquez ici">
+        </form>
+        </div>
+    </div>
 </body>
 </html>
